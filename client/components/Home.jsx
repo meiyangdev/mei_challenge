92bf97ea9748a5416eeeaf8e0befb9eb0f132025
mei-yang <abbyyang66@gmail.com>
2022-03-22 10:46:32 +1300

    add sidebar content

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,12 +3,24 @@ import React from "react";
 function Home() {
 	return (
 		<>
-			<div className='content'>
+		<div className='home'>
+      <div className='content'>
 				<h1>Welcome to Kiwikickz</h1>
 				<p>
 					<img id='img' src='./images/kiwikickz.png'></img>
 				</p>
 			</div>
+			<div className='sidebar'>
+			<h2>Overview</h2>
+			<ul>
+				<li>UserStories</li>
+				<li>Tech Stack</li>
+				<li>Review</li>
+				<li>Learn More</li>
+			</ul>
+			</div>
+		</div>
+			
 		</>
 	);
 }
